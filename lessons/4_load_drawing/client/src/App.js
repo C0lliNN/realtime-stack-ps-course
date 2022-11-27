f62576a99917b57fe94d636d85757095585ff6b8
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-11-27 14:12:43 -0300

    Finsh lesson 4

--- lessons/4_load_drawing/client/src/App.js
+++ lessons/4_load_drawing/client/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Drawing from './Drawing';
 import DrawingForm from './DrawingForm';
 import DrawingList from './DrawingList';
 
 class App extends Component {
+  state = {
+
+  }
+
+  selectDrawing = (drawing) => {
+    this.setState({ selectedDrawing: drawing })
+  }
+
   render() {
+    let ctrl = (
+      <div>
+        <DrawingForm />
+        <DrawingList selectDrawing={this.selectDrawing}/>
+      </div>
+    )
+
+
+    if (this.state.selectedDrawing) {
+      ctrl = <Drawing drawing={this.state.selectedDrawing} key={this.state.selectedDrawing.id} />
+    }
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Our awesome drawing app</h2>
         </div>
-
-        <DrawingForm />
-
-        <DrawingList />
+        {ctrl}
       </div>
     );
   }
