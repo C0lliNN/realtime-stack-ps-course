b48a8d5719044126d8c6789d8c7f859658bad8a2
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-11-27 14:49:29 -0300

    Finsh lesson 6

--- lessons/6_drawing_lines_batch/client/src/api.js
+++ lessons/6_drawing_lines_batch/client/src/api.js
@@ -1,22 +1,31 @@
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8000');
+import openSocket from "socket.io-client";
+import Rx from "rxjs/Rx";
+const socket = openSocket("http://localhost:8000");
 
 function subscribeToDrawings(cb) {
-  socket.on('drawing', drawing => cb(drawing));
-  socket.emit('subscribeToDrawings');
+  socket.on("drawing", (drawing) => cb(drawing));
+  socket.emit("subscribeToDrawings");
 }
 
 function createDrawing(name) {
-  socket.emit('createDrawing', { name });
+  socket.emit("createDrawing", { name });
 }
 
 function publishLine({ drawingId, line }) {
-  socket.emit('publishLine', { drawingId, ...line });
+  socket.emit("publishLine", { drawingId, ...line });
 }
 
 function subscribeToDrawingLines(drawingId, cb) {
-  socket.on(`drawingLine:${drawingId}`, line => cb(line));
-  socket.emit('subscribeToDrawingLines', drawingId);
+  const lineStream = Rx.Observable.fromEventPattern(
+    (h) => socket.on(`drawingLine:${drawingId}`, h),
+    (h) => socket.off(`drawingLine:${drawingId}`, h)
+  );
+
+  const bufferedStream = lineStream.bufferTime(100).map((lines) => ({ lines }));
+
+  bufferedStream.subscribe((linesEvent) => cb(linesEvent));
+
+  socket.emit('subscribeToDrawingLines', drawingId)
 }
 
 export {
