acb76d4d4fc4f3747255a6f0835eb6477c1363d0
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-11-27 15:18:40 -0300

    Finsh lesson 9

--- lessons/8_disconnect_reconnect_subscriber/client/src/Drawing.js
+++ lessons/8_disconnect_reconnect_subscriber/client/src/Drawing.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import Canvas from 'simple-react-canvas';
-import { publishLine, subscribeToDrawingLines } from './api';
+import React, { Component } from "react";
+import Canvas from "simple-react-canvas";
+import { publishLine, subscribeToDrawingLines } from "./api";
 
 class Drawing extends Component {
   state = {
     lines: [],
-  }
+  };
 
   componentDidMount() {
     subscribeToDrawingLines(this.props.drawing.id, (linesEvent) => {
@@ -22,14 +22,14 @@ class Drawing extends Component {
       drawingId: this.props.drawing.id,
       line,
     });
-  }
+  };
 
   render() {
-    return (this.props.drawing) ? (
-      <div
-        className="Drawing"
-      >
-        <div className="Drawing-title">{this.props.drawing.name}</div>
+    return this.props.drawing ? (
+      <div className="Drawing">
+        <div className="Drawing-title">
+          {this.props.drawing.name} ({this.state.lines.length})
+        </div>
         <Canvas
           onDraw={this.handleDraw}
           drawingEnabled={true}
